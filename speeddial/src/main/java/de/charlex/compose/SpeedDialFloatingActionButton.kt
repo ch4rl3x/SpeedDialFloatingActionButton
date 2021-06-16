c02e528a2c3c0901a87dc6774513d02b00ba90de
Alexander Karkossa <alexander.karkossa@googlemail.com>
2021-06-16 20:30:10 +0200

    build: switched to compose 1.0.0-beta09

--- speeddial/src/main/java/de/charlex/compose/SpeedDialFloatingActionButton.kt
+++ speeddial/src/main/java/de/charlex/compose/SpeedDialFloatingActionButton.kt
@@ -1,30 +1,17 @@
 package de.charlex.compose
 
 import androidx.annotation.DrawableRes
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.FloatingActionButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.contentColorFor
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -39,7 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEachIndexed
 
-@OptIn(ExperimentalAnimationApi::class)
+@ExperimentalMaterialApi
 @Composable
 fun SpeedDialFloatingActionButton(
     modifier: Modifier = Modifier,
@@ -147,17 +134,17 @@ fun SpeedDialFloatingActionButton(
                     if (showLabels) {
 
                         Surface(
-                            modifier = Modifier.clickable(
-                                    interactionSource = interactionSource,
-                                    indication = rememberRipple()
-                                ) {
-                                    onClick(data)
-                                }
+                            modifier = Modifier
                                 .alpha(speedDialAlpha[correctIndex].value)
                                 .scale(speedDialScale[correctIndex].value),
                             shape = MaterialTheme.shapes.small,
                             color = speedDialBackgroundColor,
-                            contentColor = speedDialContentColor
+                            contentColor = speedDialContentColor,
+                            onClick = {
+                                onClick(data)
+                            },
+                            interactionSource = interactionSource,
+                            indication = rememberRipple()
                         ) {
                             Text(
                                 modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp),
@@ -234,6 +221,7 @@ fun SpeedDialFloatingActionButton(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Preview
 @Composable
 fun SpeedDialPreview() {
